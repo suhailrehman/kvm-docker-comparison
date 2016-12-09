696db48eac317498daeeac8b041fcc09922c4faf
Mohammed Suhail Rehman <suhail@linux3.cs.uchicago.edu>
2016-12-09 00:33:42 -0600

    Sysbench now uses environement variables

--- mysql/prepare.sh
+++ mysql/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sysbench --test=oltp --oltp-table-size=2000000 --mysql-db=benchmark --mysql-host=localhost --mysql-user=root --mysql-port=3306 --mysql-password=$MYSQLPASS cleanup
-sysbench --test=oltp --oltp-table-size=2000000 --mysql-db=benchmark --mysql-host=localhost --mysql-user=root --mysql-port=3306 --mysql-password=$MYSQLPASS prepare
+$SYSBENCH --test=$TEST --oltp-table-size=2000000 --mysql-db=benchmark --mysql-host=$MYSQLHOST --mysql-user=root --mysql-port=3306 --mysql-password=$MYSQLPASS cleanup
+$SYSBENCH --test=$TEST --oltp-table-size=2000000 --mysql-db=benchmark --mysql-host=$MYSQLHOST --mysql-user=root --mysql-port=3306 --mysql-password=$MYSQLPASS prepare
 
 
--- mysql/run.sh
+++ mysql/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+mkdir ~/results
+
 runId=0
-while [ $runId -lt 10 ]
+while [ $runId -lt 1 ]
 do
 				numCli=10
 				while [ $numCli -lt 180 ]
 				do
-								sysbench --test=oltp --oltp-table-size=2000000 --mysql-db=benchmark --mysql-host=localhost --mysql-user=$MYSQLHOST --mysql-port=3306 --mysql-password=$MYSQLPASS  --max-time=60 --oltp-read-only=on --max-requests=0 --num-threads=$numCli run
+								$SYSBENCH --test=$TEST --oltp-table-size=2000000 --mysql-db=benchmark --mysql-host=$MYSQLHOST --mysql-user=root --mysql-port=3306 --mysql-password=$MYSQLPASS  --max-time=60 --oltp-read-only=on --max-requests=0 --num-threads=$numCli run
 								sleep 5
 								numCli=$(($numCli + 10))
-				done > ~/results-2000000-native-$runId
+				done > ~/results/results-2000000-native-$runId
 				runId=$(($runId + 1))
 done
